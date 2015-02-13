3befe8cd687454bc52314c7f826b70d5bcacc91d
Ryan Hamblin <ryanhca@gmail.com>
2015-02-12 20:03:11 -0700

    initial commit
    cheat sheet for students

---
+++ js/parseService.js
@@ -0,0 +1,56 @@
+var app = angular.module('chatroom');
+
+app.service('parseService', function($http, $q){
+  //Here you'll need to create two methods. One called postData and the other called getData.
+
+  //On the lines below create a getData method. This method will retrieve data from the parse backend.
+  //The url for the get request should be 'https://api.parse.com/1/classes/chat?order=-createdAt'
+  //Be sure to return whatever gets returned from $http so you can call .then in your controller.
+    this.getParseData = function(){
+      return $http({
+        method: "GET",
+        url: "https://api.parse.com/1/classes/chat?order=-createdAt"
+      }).then(function(response){
+          return response.data;
+      });
+    }
+
+    
+        this.postData = function(message){
+          // do an $http request to add a message to our parse backend
+          return $http({
+            method: 'POST',
+            url: 'https://api.parse.com/1/classes/chat', 
+            params: {sort: 'ascending'},
+            data: {'text': message}
+          })
+        };
+
+
+        this.getData = function(){
+
+        return $http({
+          'method': 'get',
+          'url': "https://api.parse.com/1/classes/chat?order=-createdAt"
+        }).then(function(res){
+          return res.data.results
+        })
+      }
+});
+  //On the line below create the postData method. This method will add data to the parse backend.
+  //The url for the request needs to be 'https://api.parse.com/1/classes/chat'
+  //Because we're making a POST request, we need a way to tell parse the data we want to give it, 
+    //in your $http call (along with url and method) have a data property which has a value that is equal to another object which a key of text and a value of the message being passed to parse. IE data: {text: yourMessage} 
+  //Also, remember that $http returns a promise. So if you return the whole $http call (return $http(...)), you can then use .then in your controller.
+  /* IGNORE THE PARAGRAPH BELOW! WE DIDN'T COVER THIS TODAY */
+     /*Also, we want to be able to manipulate the data we get back from parse before we pass it back to our controller. The problem is 
+     when we're making a request, we don't know when the response will come back. This sound like a perfect use case for a promise. We can 
+     create a promise that resolves with the correct data from parse whenever our $http get requests finishes. 
+     Because of the way parse sends back their data, let's resolve our promise with data.data.results if you had the original parameter be date like below.
+    .then(function(data){
+      deferred.resolve(data.data.results)
+    })
+  */
+  
+  
+
